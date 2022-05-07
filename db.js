0c6023b86477545ad75282646dcc26cc118ccf77
DeusDrizzyy <66096216+DeusDrizzyy@users.noreply.github.com>
2022-05-07 00:02:31 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,27 +1,30 @@
-const { Message } = require('discord.js')
-require('colors')
-const mongoose = require ('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('colors');
+
 mongoose.connect('', {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useFindAndModify: false
-
+    useFindAndModify: false,
 }).catch(error => {
-    console.log(`[DB] Nao foi possivel conectar na mongoose, fechando conexão! Erro: ${error.message}`.red)
-})
-console.log('[DB] Database conectada!'.green)
+    console.log(`[DB] Nao foi possivel conectar na mongoose, fechando conexão! Erro: ${error.message}`.red);
+});
+
+console.log('[DB] Database conectada!'.green);
+
 let mutes = new Schema({
-    roleid: { type: String},
-    memberid: { type: String},
-    guildId: { type: String},
-})
+    roleid: { type: String },
+    memberid: { type: String },
+    guildId: { type: String },
+});
+
 let xp = new Schema({
     xp: { type: Number },
     userID: { type: String },
     guildID: { type: String },
     level: { type: Number },
-})
+});
+
 let canais = new Schema({
     group: { type: String },
     groupwelcome: { type: String },
@@ -32,30 +35,35 @@ let canais = new Schema({
     msg1: { type: String },
     msg2: { type: String },
     role: { type: String },
-    grouplog: {type: String},
-    channellogs: {type: String},
-    logs: {type: Array},
-    xpc: { type: String }
-})
+    grouplog: { type: String },
+    channellogs: { type: String },
+    logs: { type: Array },
+    xpc: { type: String },
+});
+
 let reCaptch = new Schema({
     groupid: { type: String },
     capactivy: { type: String },
     code: { type: String },
     member: { type: String },
     role: { type: String },
-})
+});
+
 let punir = new Schema({
     punid: { type: String },
     motivo: { type: String },
-})
+});
+
 let premium = new Schema({
     groupid: { type: String },
-    memberid: { type: String},
-})
+    memberid: { type: String },
+});
+
 let porn = new Schema({
     groupid: { type: String },
-    channel: { type: String},
-})
+    channel: { type: String },
+});
+
 let koins = new Schema({
     id: { type: String },
     coinsc: { type: Number },
@@ -68,97 +76,107 @@ let koins = new Schema({
     robdown: { type: Number },
     casado1: { type: String },
     casado2: { type: String },
-})
+});
+
 let loja = new Schema({
     consumidor: { type: String },
     produtos: { type: Array },
-    cooldown: {type: Number}
-})
+    cooldown: { type: Number },
+});
+
 let rep = new Schema({
     id: { type: String },
     reps: { type: Number },
-    membro: { type: String},
-    cooldown: {type: Number}
-})
+    membro: { type: String },
+    cooldown: { type: Number },
+});
+
 let repcooldown = new Schema({
-    membro: {type: String},
-    cooldown: {type: Number}
-})
+    membro: { type: String },
+    cooldown: { type: Number },
+});
+
 let sorteio = new Schema({
-    guildID: {type: String},
-    end: {type: Boolean},
-    channel: {type: String},
-    title: {type: String},
-    winners: {type: Number},
-    messageID: {type: String},
-    participants: {type: Array},
-    timestamp: {type: Number}
-})
+    guildID: { type: String },
+    end: { type: Boolean },
+    channel: { type: String },
+    title: { type: String },
+    winners: { type: Number },
+    messageID: { type: String },
+    participants: { type: Array },
+    timestamp: { type: Number },
+});
+
 let langs = new Schema({
-    guildID: {type: String},
-    lang: {type: String}
-})
+    guildID: { type: String },
+    lang: { type: String },
+});
+
 let setcommand = new Schema({
-    id: {type: String},
-    command: {type: String},
-    reply: {type: String}
-})
+    id: { type: String },
+    command: { type: String },
+    reply: { type: String },
+});
+
 let webhook = new Schema({
-    id: {type: String},
-    idweb: {type: String},
-    token: {type: String},
-    channelid: {type: String},
-    guildid: {type: String},
-})
+    id: { type: String },
+    idweb: { type: String },
+    token: { type: String },
+    channelid: { type: String },
+    guildid: { type: String },
+});
+
 let res = new Schema({
-    pergunta: {type: String},
-    perguntatolower: {type: String},
-    respostas: {type: Array},
-    especialistas: {type: Boolean},
-    categoria: {type: String},
-    autor: {type: String}
-})
+    pergunta: { type: String },
+    perguntatolower: { type: String },
+    respostas: { type: Array },
+    especialistas: { type: Boolean },
+    categoria: { type: String },
+    autor: { type: String },
+});
+
 let emp = new Schema({
-    name: {type: String},
-    nametolower: {type: String},
-    jornalistas: {type: Array, default: []},
-    dono: {type: String},
-    materias: {type: Array},
-    espectadores: {type: Number, default: 0},
-    espectadorestotal: {type: Number, default: 0},
-    lang: {type: String},
-})
-let muts = new mongoose.model('muteds', mutes)
-exports.muteds = muts
-let xpa = new mongoose.model('xps', xp)
-exports.xps = xpa
-let koin = new mongoose.model('coins', koins)
-exports.coins = koin
-let cahc = new mongoose.model('idgr', canais)
-exports.idgr = cahc
-let pram  = new mongoose.model('premi', premium)
-exports.premi = pram
-let cap  = new mongoose.model('cap', reCaptch)
-exports.cap = cap
-let mole  = new mongoose.model('ban', punir)
-exports.ban = mole
-let cons  = new mongoose.model('consu', loja)
-exports.consu = cons
-let repa  = new mongoose.model('reps', rep)
-exports.reps = repa
-let repc  = new mongoose.model('repsc', repcooldown)
-exports.repsc = repc
-let gi  = new mongoose.model('give', sorteio)
-exports.give = gi
-let lang  = new mongoose.model('lgs', langs)
-exports.lgs = lang
-let set = new mongoose.model('sets', setcommand)
-exports.sets = set
-let web = new mongoose.model('webs', webhook)
-exports.webs = web
-let ar = new mongoose.model('res', res)
-exports.res = ar
-let empa = new mongoose.model('empr', emp)
-exports.empr = empa
-let por = new mongoose.model('can',porn)
-exports.can = por
+    name: { type: String },
+    nametolower: { type: String },
+    jornalistas: { type: Array, default: [] },
+    dono: { type: String },
+    materias: { type: Array },
+    espectadores: { type: Number, default: 0 },
+    espectadorestotal: { type: Number, default: 0 },
+    lang: { type: String },
+});
+
+let muts = new mongoose.model('muteds', mutes);
+exports.muteds = muts;
+let xpa = new mongoose.model('xps', xp);
+exports.xps = xpa;
+let koin = new mongoose.model('coins', koins);
+exports.coins = koin;
+let cahc = new mongoose.model('idgr', canais);
+exports.idgr = cahc;
+let pram = new mongoose.model('premi', premium);
+exports.premi = pram;
+let cap = new mongoose.model('cap', reCaptch);
+exports.cap = cap;
+let mole = new mongoose.model('ban', punir);
+exports.ban = mole;
+let cons = new mongoose.model('consu', loja);
+exports.consu = cons;
+let repa = new mongoose.model('reps', rep);
+exports.reps = repa;
+let repc = new mongoose.model('repsc', repcooldown);
+exports.repsc = repc;
+let gi = new mongoose.model('give', sorteio);
+exports.give = gi;
+let lang = new mongoose.model('lgs', langs);
+exports.lgs = lang;
+let set = new mongoose.model('sets', setcommand);
+exports.sets = set;
+let web = new mongoose.model('webs', webhook);
+exports.webs = web;
+let ar = new mongoose.model('res', res);
+exports.res = ar;
+let empa = new mongoose.model('empr', emp);
+exports.empr = empa;
+let por = new mongoose.model('can', porn);
+exports.can = por;
