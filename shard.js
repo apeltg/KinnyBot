a3c90afbd6b3156d4b501357f3e6bafd18ddf091
DeusDrizzyy <66096216+DeusDrizzyy@users.noreply.github.com>
2022-05-06 23:16:26 -0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,22 +1,22 @@
 const { ShardingManager } = require('discord.js');
-const { token, tokenc } = require('./config.json');
+const { token } = require('./config.json');
 const colors = require('colors')
- 
+
 const shards = new ShardingManager('./bot.js', {
-  respawn: true,
-  totalShards: 'auto',
-  token,
+    respawn: true,
+    totalShards: 'auto',
+    token,
 });
 
 shards.on('shardCreate', (shard) => {
-  console.log(`[SHARD] Iniciando a shard de id ${shard.id}`.green);
+    console.log(`[SHARD] Iniciando a shard de id ${shard.id}`.green);
 });
-
 shards.spawn();
+
 process.on('uncaughtException', (err) => {
-  console.error(err);
+    console.error(err);
 });
 
 process.on('unhandledRejection', (err) => {
-  console.error(err);
+    console.error(err);
 });
