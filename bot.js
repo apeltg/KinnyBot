d43cdbd5b4ba9e09d2867e7c62842f7e713c57ae
DeusDrizzyy <66096216+DeusDrizzyy@users.noreply.github.com>
2022-05-06 23:39:21 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
-const db = require('./db.js')
-const { Client, Collection, MessageEmbed} = require('discord.js')
-const { tokenc, tokenAPI, token, spotify, nodesLava, webhookPasswordTopGG } = require("./config.json")
+const db = require('./db.js');
+const { Client, Collection, MessageEmbed } = require('discord.js');
+const { tokenc, tokenAPI, spotify, nodesLava, webhookPasswordTopGG } = require("./config.json");
 const DBL = require('top.gg');
 const dbl = new DBL(tokenAPI, { webhookPort: 25685, webhookAuth: webhookPasswordTopGG });
-const { Vulkava } = require('vulkava')
-const { readdir } = require('fs');
+const { Vulkava } = require('vulkava');
+const { readdir } = require('node:fs');
+
 const client = new Client({
     shardId: process.argv[1],
     shardCount: process.argv[2],
@@ -26,26 +27,30 @@ const client = new Client({
         }
     }
 });
+
 const manager = new Vulkava({
     nodes: nodesLava,
     sendWS: (guildId, payload) => {
         client.guilds.cache.get(guildId)?.shard.send(payload)
     },
     spotify: spotify
-},
-)
+});
+
 dbl.webhook.on('ready', hook => {
     console.log(`[WEBHOOK TOP.GG] Web hook logado! no ip ${hook.hostname} e na porta ${hook.port}`.green);
 });
+
 dbl.webhook.on('vote', async vote => {
-    const find = await db.coins.findOne({ id: vote.user })
-    !find ? await db.coins.create({ id: vote.user, coinsc: 5000, coinsb: 0 }) : await db.coins.findOneAndUpdate({ id: vote.user }, { coinsc: find.coinsc + 5000 })
-    client.users.cache.get(vote.user).send('Detectamos que você fez 1 voto na top.gg! Serio obrigado mesmo. E você acaba de receber 5K de koins!').catch(error => {
-        console.log('Um erro ocorreu ao anunciar para o ' + client.users.cache.get(vote.user).username)
-    })
-    client.channels.cache.get('821013595822620673').send(`O membro ${client.users.cache.get(vote.user).username} votou na top.gg e recebe 5K de koins! Quer essa quantia tambem? Então vote na top.gg! https://top.gg/bot/750384014388494376 !`)
+    const find = await db.coins.findOne({ id: vote.user });
+    !find ? await db.coins.create({ id: vote.user, coinsc: 5000, coinsb: 0 }) : await db.coins.findOneAndUpdate({ id: vote.user }, { coinsc: find.coinsc + 5000 });
+    client.users.cache.get(vote.user).send('Detectamos que você fez 1 voto na top.gg! Serio obrigado mesmo. E você acaba de receber 5K de koins!').catch(() => {
+        console.log('Um erro ocorreu ao anunciar para o ' + client.users.cache.get(vote.user).username);
+    });
+
+    client.channels.cache.get('821013595822620673').send(`O membro ${client.users.cache.get(vote.user).username} votou na top.gg e recebeu 5K de koins! Quer essa quantia tambem? Então vote na top.gg! https://top.gg/bot/750384014388494376 !`);
 });
-['commands', 'aliases'].forEach(f => client[f] = new Collection());
+
+['commands', 'aliases'].forEach(f => (client[f] = new Collection()));
 ['comandos', 'eventos'].forEach(f => require(`./src/handlers/${f}`)(client));
 readdir('./src/eventos/music', (err, arrives) => {
     if (err) return console.log(err);
@@ -53,24 +58,26 @@ readdir('./src/eventos/music', (err, arrives) => {
         const event = require(`./src/eventos/music/${arrive}`);
         const eventName = arrive.split('.')[0];
         manager.on(eventName, event.bind(null, client));
-    })
-})
+    });
+});
+
 client.login(tokenc);
-client.manager = manager
+client.manager = manager;
+
 process.on('uncaughtException', (err) => {
     const embed = new MessageEmbed()
         .setTitle('Um erro interno ocorreu!')
-        .setDescription(`Erro: ${err.message}`)
+        .setDescription(`Erro: ${err.message}`);
 
-    client.channels.cache.get('873719017616068638')?.send({ embeds: [embed] })
+    client.channels.cache.get('873719017616068638')?.send({ embeds: [embed] });
     console.error(err);
 });
 
 process.on('unhandledRejection', (err) => {
     const embed = new MessageEmbed()
         .setTitle('Um erro interno ocorreu!')
-        .setDescription(`Erro: ${err.message}`)
+        .setDescription(`Erro: ${err.message}`);
 
-    client.channels.cache.get('873719017616068638')?.send({ embeds: [embed] })
+    client.channels.cache.get('873719017616068638')?.send({ embeds: [embed] });
     console.error(err);
 });
